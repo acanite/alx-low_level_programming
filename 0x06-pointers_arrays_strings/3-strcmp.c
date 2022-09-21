d9fc952e10a62ab1bef4832d0354b5c2b3e83593
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2022-09-21 19:48:31 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * _strcmp - This function compares two string
- * @s1: The first string to compare
- * @s2: The second string to get printed
- * Return: The diff between the two strings
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
  */
 
-int _strcmp(char *s1, char *s2)
+void reverse_array(int *a, int n)
 {
-	int i = 0, op = 0;
+	int i;
+	int temp;
 
-	while (op == 0)
+	for (i = 0; i < n--; i++)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
-
-	return (op);
 }
