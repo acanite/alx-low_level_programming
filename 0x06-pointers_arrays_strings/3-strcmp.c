0022c14487bdf41471d917b377e8f60d96d93aac
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2022-09-21 19:46:15 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
- * @s1: First string
- * @s2: Second string
- * Return: s1[i] - s2[i]
+ * _strcmp - This function compares two string
+ * @s1: The first string to compare
+ * @s2: The second string to get printed
+ * Return: The diff between the two strings
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	int i = 0, op = 0;
+
+	while (op == 0)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
 		i++;
 	}
-	return (0);
+
+	return (op);
 }
