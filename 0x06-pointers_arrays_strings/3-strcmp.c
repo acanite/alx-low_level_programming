f6270270a61c9815990cf9b6db87b958b43aa638
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2022-09-21 20:24:04 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,47 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
+ * _strcmp - compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: s1[i] - s2[i]
  */
-void print_array(int *a, int n)
+
+int _strcmp(char *s1, char *s2)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    /**print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    //print_array(a, sizeof(a) / sizeof(int));
-    */
-     reverse_array(a, sizeof(a) / sizeof(int));
-
-    return (0);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
