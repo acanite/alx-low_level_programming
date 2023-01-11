61909ab878fa466e7ba739292b63b9fe91fdf3e8
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2023-01-12 00:34:27 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table.
- * @ht: the hash table
- *
- * Return: Nothing
- */
+ * hash_table_delete - deletes a hash table
+ * @ht: is the hash table
+**/
 
 void hash_table_delete(hash_table_t *ht)
 {
